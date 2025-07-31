1257211173673f904b23b2ca4d82347dd464130d
Jan Härkönen <jan.harkonen@gmail.com>
2025-07-31 23:57:40 +0300

    add workspace switching, update tmux pane navigation, xbindkeys for chromium tabs

--- makesymlink.sh
+++ makesymlink.sh
@@ -1,3 +1,5 @@
 sudo ln ~/.bashrc .
 sudo ln ~/.tmux.conf .
 sudo ln ~/.vimrc .
+sudo ln ~/.inputrc .
+sudo ln ~/.xbindkeysrc .
--- restart.sh
+++ restart.sh
@@ -1,1 +1,10 @@
 tmux source ~/.tmux.conf
+bind -f ~/.inputrc
+
+#this works for ubuntu at least
+for i in {1..9}; do
+  gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Alt>$i']"
+done
+
+killall xbindkeys 2>/dev/null
+xbindkeys
